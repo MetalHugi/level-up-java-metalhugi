7de9e1ef1eb443e0b4a7cdc971b03bb2e0cd5ec0
MetalHugi <metalhugi@gmail.com>
2022-11-17 16:43:35 +0000

    Is password complex?

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,31 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
+
+        return password.length() >= 6 
+            && password.matches(".*\\d.*")
+            && password.matches(".*[a-z].*")
+            && password.matches(".*[A-Z].*");
+
+/*        if (password.length() < 6) {
         return false;
+       }
+       boolean hasUpper = false;
+       boolean hasLower = false;
+       boolean hasNumber = false;
+
+       for (int i = 0; i < password.length(); i++) {
+        char currentChar = password.charAt(i);
+        if(Character.isDigit(currentChar)){
+            hasNumber = true;
+        } else if (Character.isLowerCase(currentChar)){
+            hasLower = true;
+        } else if (Character.isUpperCase(currentChar)){
+            hasUpper = true;
+        }
+       }
+
+        return hasLower && hasUpper && hasNumber; */
     }
 
     public static void main(String[] args) {
